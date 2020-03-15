ca4d21cf8c64e54194dbd33f24daaa4e98d6e592
Jimmy Engström <jimmy@apeoholic.se>
2020-03-15 02:42:09 +0100

    Added updating the title

---
+++ EngstromJimmySe/Services/AppService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace EngstromJimmySe.Services
+{
+    public class AppStateService : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
--- EngstromJimmySe/Startup.cs
+++ EngstromJimmySe/Startup.cs
@@ -30,6 +30,7 @@ namespace EngstromJimmySe
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<BlogService>();
+            services.AddScoped<AppStateService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
